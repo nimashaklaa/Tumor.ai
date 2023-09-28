f993ea8a30fbd9281565b63b916d8d792be61de5
CoderNavinda <72337893+CoderNavinda@users.noreply.github.com>
2023-09-28 18:02:59 +0530

    contact us backend done

--- flask-server/server.py
+++ flask-server/server.py
@@ -9,125 +9,74 @@ import os
 import numpy as np
 import tensorflow as tf
 from keras.models import model_from_json
+import base64
+from email.mime.text import MIMEText
+import json 
+import time
+import uuid
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.application import MIMEApplication
 
 
-#gmail functionality
-from google.oauth2 import service_account
-import googleapiclient.discovery
 app = Flask(__name__)
 CORS(app,supports_credentials=True)
 
-
-
-
-# Load OAuth2 credentials from the JSON file
-creds = None
-SCOPES = ['https://www.googleapis.com/auth/gmail.send']
-
-creds = service_account.Credentials.from_service_account_file(
-    'credentials.json', scopes=SCOPES)
-
-# Create a Gmail API service
-service = googleapiclient.discovery.build('gmail', 'v1', credentials=creds)
 
 #initializing MongoDb Database
 mongo_uri = "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.10.6"
 client = MongoClient(mongo_uri)
 
 
+# Email configuration
+smtp_server = 'smtp.gmail.com'
+smtp_port = 587
+smtp_username = 'sithnavin@gmail.com'
+smtp_password = 'mbil yntd zxeb cigc'
+subject = "Response to Your Inquiry from Tumor.ai"
 
 
 
 
-
-
-
-@app.route("/api/contactus", methods=[ "POST"])
+@app.route('/api/contactus', methods=['POST'])
 def contactus():
-    if request.method == "POST":
-        name = 'name'
-        email = 'navinda.20@gmail.com'
-        message = 'first test'
-        print(name,email,message)
-
-
-
-        message = create_message(
-            sender=email,
-            to='recipient@gmail.com',  # Replace with the recipient's email
-            subject=message,
-            message_text=f'Name: {name}\nEmail: {email}\nSubject: {message}\nMessage: {message}'
-        )
-        send_message(service, 'me', message)
-
-
-
-        return jsonify({'message': 'Message sent successfully'}), 200
-    else:
-        return jsonify({'message': 'Message not sent'}), 404
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    global smtp_server
+    try:
+
+        # Extract user input data
+        user_name = 'navinda'
+        user_email = 'navinda.20@cse.mrt.ac.lk'
+        user_message = 'test message'
+
+        # Check if any of the fields are missing
+        if not user_name or not user_email or not user_message:
+            return jsonify({'error': 'Incomplete data provided'}), 400
+
+        # Customize the email response message with the user's name
+        message = f"Thank you for reaching out, {user_name}!\n\n"  # Include the user's name in the message
+        message += "We appreciate your inquiry and will get back to you as soon as possible. "
+        message += "If you have any further questions or need immediate assistance, "
+        message += "please feel free to contact us at +94 77 39 35 182."
+        print(message)
+        # Compose the email message
+        email_msg = MIMEMultipart()
+        email_msg['From'] = smtp_username
+        email_msg['To'] = user_email
+        email_msg['Subject'] = subject
+        email_msg.attach(MIMEText(message, 'plain'))
+
+        # Create SMTP server session
+        smtp_server = smtplib.SMTP(smtp_server, smtp_port)
+        smtp_server.starttls()
+        smtp_server.login(smtp_username, smtp_password)
+        smtp_server.sendmail(smtp_username, user_email, email_msg.as_string())
+        smtp_server.quit()
+
+        return jsonify({'message': 'Email sent successfully'})
+
+    except Exception as e:
+        return jsonify({'error': f'Error sending email: {str(e)}'}), 500
 
 
 
@@ -139,69 +88,6 @@ device = "cpu"
 segment_model = None
 path = "./model_state_dict.pt"
 data_transforms = None
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
