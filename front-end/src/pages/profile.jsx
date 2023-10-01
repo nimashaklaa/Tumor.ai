37916e232c5d7279757f12a44826baae2398749e
nimashaklaa <119551535+nimashaklaa@users.noreply.github.com>
2023-10-01 21:47:54 +0530

    fixed profile+add avater imge to public

--- front-end/src/pages/profile.jsx
+++ front-end/src/pages/profile.jsx
@@ -1,9 +1,14 @@
 import React,{ useState } from 'react';
 import "./profile.css";
+import ErrorPopup from './errorPopup';
 
 function Profile() {
   //===================change image or remove image=========================================================
-  const [imageUrl, setImageUrl] = useState('/avatar.jpeg'); // Initial image URL
+  const [isEditMode, setIsEditMode] = useState(false);
+  const [imageUrl, setImageUrl] = useState('/avatar.jpg'); // Initial image URL
+  const [currentError, setCurrentError] = useState('');
+  const [showError, setShowError] = useState(false);
+  const [showSuccess, setShowSuccess] = useState(false);
 
   // Function to handle image changes
   const handleImageChange = (newImageUrl) => {
@@ -13,24 +18,11 @@ function Profile() {
   const resetImage = () => {
     setImageUrl(null);
   }
-  //====================================================enter multiple rows for quallifications upto 3 qualifications=========
-  const [qualificationList, setQualificationList] = useState(['']);
-
-  const addQualificationField = () => {
-    if (qualificationList.length < 3) {
-      setQualificationList([...qualificationList, '']);
-    }
-  };
-
-  const handleInputChange = (index, event) => {
-    const updatedList = [...qualificationList];
-    updatedList[index] = event.target.value;
-    setQualificationList(updatedList);
-  };
+  
   //====================handle data in the form==============================================================================
   const [formData, setFormData] = useState({
-    email: '',contactno:'',countrycode:'',experience:'',specialization:'',
-    firstname:'',lastname:'',pnoneno:'',DoB:'',medical:'',hospital:'',title:'',confirm:'',country:'',address1:'',address2:''
+    email: '',contactno:'',experience:'',specialization:'',
+    firstname:'',lastname:'',medical:'',hospital:'',country:''
   });
 
   const handleChange = (e) => {
@@ -40,56 +32,155 @@ function Profile() {
       [name]: value,
     });
   };
+  const validateContact = () => {
 
-  const handleSubmit = (e) => {
+    const regex = /^\d{10}$/; // Assumes a 10-digit number
+
+    if (!regex.test(formData.contactno)) {
+      setCurrentError('contact no');
+    }  else if (currentError === 'contactno') {
+      setCurrentError(''); 
+    }
+  };
+  const validateEmail = () => {
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+    if (!emailRegex.test(formData.email)) {
+      setCurrentError('email');
+    }  else if (currentError === 'email') {
+      setCurrentError('');
+    }
+  };
+  const handleProfile = (e) => {
     e.preventDefault();
+      validateContact();
+      validateEmail();
+      
+      if (currentError) {
+      setShowError(true);
+      setShowSuccess(false); // Hide success message if there's an error
+      return;
+    }
     // Add authentication logic 
     console.log('Submitted data:', formData);
+    //setIsEditMode(false); // Exit edit mode after saving
+    // Simulate a successful save for demonstration purposes
+    // In a real application, you would send the data to your server and handle the response
+    // Here, we'll just show a success message after a brief delay
+    setTimeout(() => {
+      setShowSuccess(true);
+      setIsEditMode(false); // Exit edit mode after saving
+    }, 1000); // Delay for 1 second
+
+    // Clear success message after a few seconds
+    setTimeout(() => {
+      setShowSuccess(false);
+    }, 5000); // Hide success message after 5 seconds
+  
+    
+    
   };
+  
+  const toggleEditMode = () => {
+    setIsEditMode(!isEditMode);
+  };
+
   //=============================================================================================================================
   return (
     <div className="profile">
-      <div onSubmit={handleSubmit} className="form" >
-      <div className="div">
+      <div onSubmit={handleProfile} className="form" >
+    
         <div className="text-wrapper">My Profile</div>
+
         <div className="overlap-group">
-          <div className="text-wrapper-2">Country</div>
-          <div className="text-wrapper-3">Last Name</div>
+        <div className="text-wrapper-1">First Name</div>
+          <div className="text-wrapper-2">Last Name</div>
+          <div className="text-wrapper-3">Country</div>
           <div className="text-wrapper-4">Contact Number</div>
-          <div className="overlap">
-            <div className="text-wrapper-5">First Name</div>
-          </div>
-          <div className="text-wrapper-6">Title</div>
-          <div className="text-wrapper-7">Country Code</div>
-          <div className="text-wrapper-8">Address Line 1</div>
-          <div className="text-wrapper-9">Email Address</div>
-          <div className="text-wrapper-10">Personal Information</div>
-          <input className="input-1" type="text" name="title" placeholder="Medical Officer / Doctor/..." value={formData.title} onChange={handleChange}required  />
-          <input className="input-2" type="text" name="country code" placeholder="+94" value={formData.countrycode} onChange={handleChange} required  />
-          <input className="input-3" type="text" name="country" placeholder="Sri Lanka" value={formData.country} onChange={handleChange} required  />
-          <input className="input-4" type="text" name="Address1" placeholder="primary location information: street address" value={formData.address1} onChange={handleChange}required  />
-          <div className="text-wrapper-11">Address Line 2</div>
-          <input className="input-5" type="text" name="firstname" placeholder="Sally B." value={formData.firstname} onChange={handleChange} required  />
-          <input className="input-6" type="tel" name="contactNo" placeholder="+94789243743" pattern="[0-9]{10}" value={formData.contactno} onChange={handleChange} required  />
-          <input className="input-7" type="text" name="Lastname" placeholder="Alcott" value={formData.lastname} onChange={handleChange}required/>
-          <input className="input-8" type="email" name="email" placeholder="sallyAlcott9@gmail.com" value={formData.email} onChange={handleChange}required/>
-          <input className="input-9" type="text" name="address2" placeholder="Additional Address Information" value={formData.address2} onChange={handleChange}required/>
+          <div className="text-wrapper-5">Email Address</div>
+          <div className="text-wrapper-6">Personal Information</div>
+          <div className="text-wrapper-7">Working Experience</div>
+
+          <div className="text-wrapper-8">Experiences</div>
+          <div className="text-wrapper-9">Specialization</div>
+          <div className="text-wrapper-10">Hospital or Clinic Affiliation</div>
+        {isEditMode ? (
+          <>
+          <input className="input-1" type="text" name="firstname"  value={formData.firstname} onChange={handleChange} readOnly={!isEditMode}required  />
+          <input className="input-2" type="text" name="lastname" value={formData.lastname} onChange={handleChange}readOnly={!isEditMode}required/>
+          <select className="input-3" type="text" name="country"  value={formData.country} onChange={handleChange} readOnly={!isEditMode}required  >
+            <option value="" disabled hidden>Select the  Country</option>
+            <option value="Sri Lanka">Sri Lanka</option>
+            <option value="United States">United States</option>
+            <option value="Canada">Canada</option>
+            <option value="United Kingdom">United Kingdom</option>
+            <option value="Australia">Australia</option>
+            <option value="Germany">Germany</option>
+            <option value="France">France</option>
+            <option value="Spain">Spain</option>
+            <option value="Italy">Italy</option>
+            <option value="Netherlands">Netherlands</option>
+            <option value="Japan">Japan</option>
+            <option value="China">China</option>
+            <option value="India">India</option>
+            <option value="Brazil">Brazil</option>
+            <option value="Mexico">Mexico</option> 
+            <option value="South Africa">South Africa</option>
+            <option value="Other">Other</option>
+          </select>
+          <input className="input-4" type="tel" name="contactno"  pattern="[0-9]{10}" value={formData.contactno} onChange={handleChange}onBlur={validateContact}readOnly={!isEditMode} required  />
+          <input className="input-5" type="email" name="email"  value={formData.email} onChange={handleChange}onBlur={validateEmail}readOnly={!isEditMode}required/>
+          <input className="input-10" type="text" name="hospital"  value={formData.hospital} onChange={handleChange}readOnly={!isEditMode} />
+          <input className="input-11" type="text" name="experience"  defaultValue=''value={formData.experience} onChange={handleChange}readOnly={!isEditMode}required/>
+          <select className="input-12" type="text" name="specialization"  value={formData.specialization} onChange={handleChange}readOnly={!isEditMode} >
+            <option value="" disabled selected color='#d9d9d93b'>Select a Medical specialization</option>
+            <option value="Pathologist">Pathologist</option>
+            <option value="Radiologist">Radiologist</option>
+            <option value="Medical Oncologist">Medical Oncologist</option>
+            <option value="Surgical Oncologist">Surgical Oncologist</option>
+            <option value="Hematologist">Hematologist</option>
+            <option value="Dermatologist">Dermatologist</option>
+            <option value="Neurologist">Neurologist</option>
+            <option value="Clinical Research Coordinator">Clinical Research Coordinator</option>
+            <option value="Laboratory Technician">Laboratory Technician</option>
+          </select>
+
+          </>
+          ) : (
+            <>
+          <div className="input-1">{formData.firstname}</div>
+          <div className="input-2">{formData.lastname}</div>
+          <div className="input-3">{formData.country}</div>
+          <div className="input-4">{formData.contactno}</div>
+          <div className="input-5">{formData.email}</div>
+          <div className="input-11">{formData.experience}</div>
+          <div className="input-12">{formData.specialization}</div>
+          <div className="input-10">{formData.hospital}</div>
+          </>
+          )}
+          {isEditMode ? (
+          <input className='submit-profile' type="submit" value="Save changes" onClick={handleProfile}/>
+          ) : (
+         <input className='edit-profile' type="submit" value="Edit Profile" onClick={toggleEditMode}/>
+         )}
         </div>
-        <div className="overlap-2">
-          <div className="text-wrapper-13">Sally B. Alcott</div>
-          <img className="avatar-image" src="/avatar.jpeg" alt="Avatar" />
+        <div className="overlap-profile">
+          
+          <img className="avatar-image" src="/avatar.jpg" alt="Avatar" />
           
           <div>
           {imageUrl ? (
           // Display the selected image
           <div>
             <img src={imageUrl} alt="Avatar" className="avatar-image" />
-            <button onClick={resetImage} className="remove-button" style={{ display: 'block', marginTop: '286px',marginLeft:'110px' }}>Change Image</button>
+            <input onClick={resetImage} className='changeImage' type="submit" value="Edit Image" />
+           
           </div>
         ) : (
           // Display the file input when no image is selected
           <div>
             <input
+              
               type="file"
               accept="image/*"
               onChange={(e) => {
@@ -99,52 +190,39 @@ function Profile() {
                   handleImageChange(imageUrl);
                 }
               }}
+              style={{ display: 'block', marginTop: '286px',left:'100px',position:'relative',height:'30px'}}
             />
         </div>
       )}
           </div>
-          <div className="physiatrist">
-            <br />
-            physiatrist
-          </div>
+          
+         
         </div>
-        <div className="overlap-3">
-          <div className="text-wrapper-14">Experiences</div>
-          <div className="text-wrapper-15">Specialization</div>
-          <div className="text-wrapper-16">Qualifications</div>
-          <div className="text-wrapper-17">Hospital or Clinic Affiliation</div>
-          <div className="text-wrapper-18">Working Experience</div>
-          <input className="input-10" type="text" name="hospital" placeholder="Current Hospital/Clinic"  value={formData.hospital} onChange={handleChange} />
-          <input className="input-11" type="text" name="exper." placeholder="Experiences" value={formData.experience} onChange={handleChange}required/>
-          
-          <input className="input-12" type="text" name="speci" placeholder="Neurology/ Psychiatry/ Radiology"  value={formData.specialization} onChange={handleChange} />
-          {/*qualifications feild=============================================================================*/}
-          <div className="center-container" style={{ margin: '160px 0 0 36px' }}>
-            {qualificationList.map((qualification, index) => (
-            <div key={index} className="qualification-row" style={{height:'300px'}}>
-              <input
-                className="input-field"
-                type="text"
-                name="certificate"
-                value={qualification.institution}
-                onChange={(e) => handleInputChange(index, e)}
-                placeholder="certificate/ award/license"
-                style={{ display: 'block', marginTop: '5px',borderRadius:'5px'  }}
-              />
-            </div>
-            ))}
-            <button type="button" onClick={addQualificationField}>Add Qualifications</button>
-          </div>
-          </div>
-         {/*=============================================================================================*/}
-          <div className="overlap-4">
-          <input type="submit" value="Save changes" />
-          </div>
-          
+      
+         
+         
+      
         
         
       </div>
-    </div>
+      {showError && (
+          <ErrorPopup
+            message={
+              currentError === 'email'
+                ? 'Please correct the email address'
+                : currentError === 'contact no'
+                ? 'Please correct the contact number'
+                : 'An error occurred during registration.'
+            }
+            onClose={() => setShowError(false)}
+          />
+        )}
+        {showSuccess && (
+        <div className="success-message">
+          Changes saved successfully!
+        </div>
+      )}
+    
     </div>
   );
 };
